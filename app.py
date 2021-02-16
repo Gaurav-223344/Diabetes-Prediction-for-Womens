a8d861ce93d527b1fdbe91e1dbd3e0eb7298fa70
Gaurav Vinayak Gaikwad <61324204+Gaurav-223344@users.noreply.github.com>
2021-02-16 18:00:04 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+
+model = pickle.load(open('model.pkl', 'rb'))
+
+
+@app.route('/',methods=['GET'])
+def Home():
+    return render_template('index.html')
+
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        Pregnancies=int(request.form['pregnant'])
+        Glucose=int(request.form['glucose'])
+        BloodPressure=int(request.form['bloodpressure'])
+        SkinThickness=int(request.form['thickness'])
+        Insulin=float(request.form['insulin'])
+        weight=float(request.form['weight'])
+        height=float(request.form['height'])
+        BMI=weight/height**2
+        DiabetesPedigreeFunction=int(request.form['pedigree'])
+        Age=int(request.form['age'])
+        
+        
+        prediction=model.predict([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
+        output=(prediction[0],2)
+        if output==0:
+            return render_template('index.html',prediction_texts="You Don't Have Diabetes")
+        else:
+            return render_template('index.html',prediction_texts="You Have Diabetes")
+    
+        
+        
+    else:
+        return render_template('index.html')
+
+    
+    
+if __name__=="__main__":
+        app.run(debug=True)
+    
+    
+# if __name__=="__main__":
+#     app.run(host='0.0.0.0',port=8080)
+        
